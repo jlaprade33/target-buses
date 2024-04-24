f215c68d4cb3f0e6053a29ca758e3cebb8713bf7
Jeff LaPrade <jlaprade01@gmail.com>
2024-04-24 13:53:48 -0600

    feat: using custom logo and stock img

--- src/components/HeaderImage/index.tsx
+++ src/components/HeaderImage/index.tsx
@@ -1,11 +1,9 @@
-//constant page header img
-const headerImg = "https://www.metrotransit.org/img/nextrip.jpg";
 
 export function HeaderImage(){
     return(
         <img 
             className="w-full h-headerImg bg-no-repeat object-cover text-font-blue font-roboto bg-center" 
-            src={headerImg}
+            src={'/stock-bus-stop-img.jpg'}
             alt='background'
         />
     )
--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,14 +1,12 @@
-//constant page header img
-const logoImg = "https://www.metrotransit.org/img/MetroTransitLogo.svg";
 
 export function PageHeader(){
     return(
         <div className="h-header">
-            {/* Setup a div here with flex for alignemnt and future header bar items*/}
+            {/* Setup a div here with flex for alignment in future header bar items*/}
             <div className="flex m-auto space-between w-4/5">
                 <img 
-                    className="h-logo w-logo mt-4" 
-                    src={logoImg}
+                    className="h-logo w-logo mt-5" 
+                    src={'/logo.svg'}
                     alt='logo'
                 />
             </div>
